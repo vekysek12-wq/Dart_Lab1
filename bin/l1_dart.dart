ae0e7e8d5581a25529d9388ad8e0bca132ffd989
Stashenko <vekysek12@gmail.com>
2026-09-10 23:56:31 +0300

    learn dart syntax and types

--- bin/l1_dart.dart
+++ bin/l1_dart.dart
@@ -1,5 +1,37 @@
-import 'package:l1_dart/l1_dart.dart' as l1_dart;
+void main() {
+ String name = "RRR";
+  int age = 20;
+  double het = 1.2;
+  bool iss = true;
+  print("Hola $name,  $age");
+  print("tebe ${age + 5}");
+  print("rost:$het' $iss ");
+  
+  var count = 0;
+  var title = "Dart";
+  print("$count: $title");
+  
+  
+  const String apname = "lab1";
+  final int styear = 2026;
+  print("$apname started in $styear");
 
-void main(List<String> arguments) {
-  print('Hello world: ${l1_dart.calculate()}!');
+
+String? city = null;
+if (city != null){
+  print(city.toUpperCase(),);
+}
+List<String> f = ["rr","tt", "yy"];
+f.add('ee');
+print(f[0]);
+print(f.length);
+
+List<String> f2 = ["rr","tt", "yy"];
+for (var f in f2){
+  print(f);
+}
+
+Map<String, dynamic> pers = {'name ': 'ARE', 'age': 20};
+print(pers['name']);
+pers['city'] = 'WAR';
 }
