92bce86cb22eda792d5dfc38129f613c0ed124f9
Stashenko <vekysek12@gmail.com>
2026-09-10 21:36:10 +0300

    Initial dart project

---
+++ bin/l1_dart.dart
@@ -0,0 +1,5 @@
+import 'package:l1_dart/l1_dart.dart' as l1_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${l1_dart.calculate()}!');
+}
---
+++ test/l1_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:l1_dart/l1_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
