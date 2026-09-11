61688e853adde54268b2322fa7ecca24e9e514a4
Stashenko <vekysek12@gmail.com>
2026-09-11 16:10:23 +0300

    learn dart functions

--- bin/l1_dart.dart
+++ bin/l1_dart.dart
@@ -1,37 +1,68 @@
+
 void main() {
- String name = "RRR";
+  var x = 4;
+
+  descPet(name: "barsek", age: 3);
+  descPet(name:  "Sharek", species: "pesek");
+
+
+  List<String> names = ["artem", "mariaa", "Evan"];
+  List<String> upper = names.map((name) => name.toUpperCase()).toList();
+  print(upper);
+
+  print(greet("arara"));
+  print(greet("ERERER"));
+  String name = "RRR";
   int age = 20;
   double het = 1.2;
   bool iss = true;
   print("Hola $name,  $age");
   print("tebe ${age + 5}");
   print("rost:$het' $iss ");
-  
+
   var count = 0;
   var title = "Dart";
   print("$count: $title");
-  
-  
+
   const String apname = "lab1";
   final int styear = 2026;
   print("$apname started in $styear");
 
+  String? city = null;
+  if (city != null) {
+    print(city.toUpperCase());
+  }
+  List<String> f = ["rr", "tt", "yy"];
+  f.add('ee');
+  print(f[0]);
+  print(f.length);
 
-String? city = null;
-if (city != null){
-  print(city.toUpperCase(),);
+  List<String> f2 = ["rr", "tt", "yy"];
+  for (var f in f2) {
+    print(f);
+  }
+
+  Map<String, dynamic> pers = {'name ': 'ARE', 'age': 20};
+  print(pers['name']);
+  pers['city'] = 'WAR';
+
+
+  print(repeat("ab"));       
+  print(repeat("ab", 3));    
 }
-List<String> f = ["rr","tt", "yy"];
-f.add('ee');
-print(f[0]);
-print(f.length);
 
-List<String> f2 = ["rr","tt", "yy"];
-for (var f in f2){
-  print(f);
+String greet(String name) {
+  return "ff,$name!";
 }
 
-Map<String, dynamic> pers = {'name ': 'ARE', 'age': 20};
-print(pers['name']);
-pers['city'] = 'WAR';
+String repeat(String text, [int times = 2]) {
+  String result = "";
+  for (int i = 0; i < times; i++) {
+    result += text;
+  }
+  return result;
+}
+
+void descPet({required String name, String species = "cate", int age = 0 }){
+  print("$name - $species, возраст $age");
 }
